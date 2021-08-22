01b522ca5e6d4288769e82135a39c0be1d945763
NicolasIannello <nicolas.iannello99@gmail.com>
2021-08-22 18:13:40 -0300

    - func repelim

--- app/controllers/turnosControllers.php
+++ app/controllers/turnosControllers.php
@@ -25,7 +25,6 @@
                 $response->getBody()->Write(json_encode("no encontrado"));
                 return $response;    
             }else{
-                //turno::ReportarEliminacion();
                 $turnos=turno::ObtenerTodos();
                 $response->getBody()->Write(json_encode($turnos));
                 return $response;    
