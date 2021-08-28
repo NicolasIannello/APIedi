2df9fa7177a735242c41b35edc833a47088bff11
NicolasIannello <nicolas.iannello99@gmail.com>
2021-08-28 19:18:04 -0300

    //env

--- app/DB/accesoDatos.php
+++ app/DB/accesoDatos.php
@@ -6,8 +6,8 @@
 
         private function __construct(){
             try {
-                $this->objetoPDO = new PDO('mysql:host='.getenv('server').':3306;dbname='.getenv('db').';charset=utf8', getenv('user') , getenv('password') , array(PDO::ATTR_EMULATE_PREPARES => false, PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
-                //$this->objetoPDO = new PDO('mysql:host=remotemysql.com:3306;dbname=rVG5xriVSC;charset=utf8', 'rVG5xriVSC', 'Vxc2D8cLx2', array(PDO::ATTR_EMULATE_PREPARES => false, PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+                //$this->objetoPDO = new PDO('mysql:host='.getenv('server').':3306;dbname='.getenv('db').';charset=utf8', getenv('user') , getenv('password') , array(PDO::ATTR_EMULATE_PREPARES => false, PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+                $this->objetoPDO = new PDO('mysql:host=remotemysql.com:3306;dbname=rVG5xriVSC;charset=utf8', 'rVG5xriVSC', 'Vxc2D8cLx2', array(PDO::ATTR_EMULATE_PREPARES => false, PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
                 $this->objetoPDO->exec("SET CHARACTER SET utf8");
             } catch (PDOException $e) {
                 print "Error: " . $e->getMessage();
